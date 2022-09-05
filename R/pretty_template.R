5e6fb4df2b801fa4d4cd3923854507e3d110b6cf
Bolin Wu <bolin.wu1026@gmail.com>
2022-09-05 15:10:47 +0200

    add output_file function and provide 'word' as an option

--- R/pretty_template.R
+++ R/pretty_template.R
@@ -2,6 +2,7 @@
 #' @description Use the skeleton.Rmd for starting analysis. This function creates a new folder "rmd" at root of project and create an rmd file with nice-looking template there.
 #' @param name name of rmd file. No need to add suffix ".Rmd".
 #' @param open should the file be opened after being created
+#' @param output_file "word" or "html", by default it is "html"
 #' @param ... arguments to be passed to \link[usethis]{use_template}
 #' @importFrom usethis use_template
 #' @importFrom here here
@@ -13,6 +14,7 @@
 #' @export
 pretty_template <-
   function(name = NULL,
+           output_file = "html",
            open = interactive(),
            ...) {
 
@@ -26,9 +28,17 @@ pretty_template <-
     dir.create("rmd", showWarnings = FALSE)
 
     # usethis::use_package("usethis")
-
+    if (output_file == "html") {
     usethis::use_template("skeleton.Rmd",
                           save_as=  paste0("rmd/", name),
                           data = list(),
                           package = "neartools", ..., open = open)
+    } else if (output_file == "word"){
+      usethis::use_template("skeleton_word.Rmd",
+                            save_as=  paste0("rmd/", name),
+                            data = list(),
+                            package = "neartools", ..., open = open)
+    } else{
+      stop("The 'output_file should be 'html' or 'word'")
+    }
   }
