3d91a011e8023ac9fc82cb5a4d7098736cd4b053
Bolin Wu <bolin.wu1026@gmail.com>
2022-10-21 14:10:11 +0200

    test merge

---
+++ R/fix_csv_na.R
@@ -0,0 +1,26 @@
+
+#' Fix the empty string in csv file
+#'
+#' When importing the csv file to MySQL workbench, if there is a cell with empty strings, the 'Wizard Import' function would skip that row.
+#' To fix this problem, we need to replace the empty string with "NA" so that we can import all records into MySQL.
+#'
+#' @param df_name A tibble in R.
+#'
+#' @note This replacement only applies to 'character' columns.
+#'
+#' @import dplyr
+#'
+#' @return A tibble with empty strings are replaced as 'NA'.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' fix_empty_string("baseline_example_Nurse_220504")
+#' }
+fix_empty_string <- function(df_name) {
+  df <- get(df_name)
+  # replace the empty string (w/o space) as NA
+  df <- df %>% mutate_if(is.character, list(~ na_if(., "")))
+  return(df)
+}
